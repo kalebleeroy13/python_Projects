87bf896627cbd6f68ff9ee16db2840cbd2f2e818
khuunter@yahoo.com <khuunter@yahoo.com>
2023-05-07 17:51:01 -0500

    intial commit

---
+++ bitmap_message.py
@@ -0,0 +1,6 @@
+"""Bitmap, Message by Al sweigart al@inventwithpython.com
+Displays a text message according to the provided bitmap image.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, artistic """
+
+import sys
