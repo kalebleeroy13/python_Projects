dd7776b59f4f6e571360c57f3c550b300784e2f8
khuunter@yahoo.com <khuunter@yahoo.com>
2023-05-07 22:50:02 -0500

    fixed print bug

--- bitmap_message.py
+++ bitmap_message.py
@@ -1,4 +1,4 @@
-"""Bitmap, Message by Al sweigart al@inventwithpython.com
+"""Bitmap, Message by Al Sweigart al@inventwithpython.com
 Displays a text message according to the provided bitmap image.
 View this code at https://nostarch.com/big-book-small-python-projects
 Tags: tiny, beginner, artistic """
@@ -31,7 +31,7 @@ bitmap = """
                     ****              **                 *******   *
                     ***                                       *    *
                     **     *                    *
-.................................................................... """
+...................................................................."""
 
 print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
 print('Enter the message to display with the bitmap.')
@@ -43,10 +43,10 @@ if message == '':
 for line in bitmap.splitlines():
     # Loop over each character in the line:
     for i, bit in enumerate(line):
-        if bit =='':
+        if bit ==' ':
             # Print an empty space since there's a space in the bitmap:
-            print('', end='')
+            print(' ', end='')
         else:
             # Print a character from the message:
             print(message[i % len(message)], end='')
-        print() # Print a newline.
+    print() # Print a newline.
