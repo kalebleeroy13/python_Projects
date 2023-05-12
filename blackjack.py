e8d3b4a373130b69cf787adb3a7fde42d852f8fd
khuunter@yahoo.com <khuunter@yahoo.com>
2023-05-12 14:11:14 -0400

    deck and dealer logic

--- blackjack.py
+++ blackjack.py
@@ -94,5 +94,115 @@ def main():
                 displayHands(playerHand, dealerHand, False)
 
                 if getHandValue(dealerHand) > 21:
-                    break # The dealer has buster.
+                    break # The dealer has busted.
                 input('Press Enter to continue...')
+                print('\n\n')
+
+        # Show the final hands:
+        displayHands(playerHand, dealerHand, True)
+
+        playerValue = getHandValue(playerHand)
+        dealerValue = getHandValue(dealerHand)
+        #Handle whether the player won, lost, or tied:
+        if dealerValue > 21:
+            print('Dealser busts! You win ${}!' .format(bet))
+            money += bet
+        elif (playerValue > 21) or (playerValue < dealerValue):
+            print('You Lost!')
+            money -= bet
+        elif playerValue > dealerValue:
+            print('You won ${}!' .format(bet))
+            money += bet
+        elif playerValue == dealerValue:
+            print('It\'s a tie, the bet is returned to you.')
+        
+        input('Press Enter to continue...')
+        print('\n\n')
+
+def getBet(maxBet):
+    """Ask the player how much they want to bet for this round."""
+    while True: #Keep asking until they enter a valid amount.
+        print('How much do you bet? (1-{}, or Quit)' .format(maxBet))
+        bet = input('>').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue # if the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet # Player entered a valid bet. 
+        
+
+
+def getDeck():
+    """Return a list of (rank,suit) tuples for all 52 cards."""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            deck.append((str(rank), suit)) #ADD the numbered cards. 
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit)) #Ad the face and ace cards.
+    random.shuffle(deck)
+    return deck
+
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if showDealerHand is False."""
+    print()
+    if showDealerHand:
+        print('Dealer:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('Dealer: ???')
+        #Hide the dealer's first card:
+        displayCards([BACKSIDE] + dealerHand[1:])
+
+    # Show the player's cards:
+    print('PLAYER:', getHandValue(playerHand))
+    displayHands(playerHand)
+
+
+def getHandValue(cards):
+    """Returns the value of the cards. Face cards are worth 10, aces are
+    worth 11 or 1 (this function picks the most suitable ace value)."""
+    value = 0
+    numberOfAces = 0
+
+    # Add the value for the non-ace cards:
+    for card in cards:
+        rank = card[0] #card is a tuple like (rank, suit)
+        if rank == 'A':
+            numberOfAces += 1
+        elif rank in ('K', 'Q', 'J'): # Face cards are worth 10 points.
+            value += 10
+        else:
+            value += int(rank) #Numbered cards are worth their number.
+
+    # Add the value for the aces:
+    value += numberOfAces #Add 1 per ace.
+    for i in range(numberOfAces):
+        # If another 10 can be added with busting, do so:
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+
+def displayCards(cards):
+    """Display all the cards in the cards list."""
+    rows = ['', '', '', '', ''] # The text to display on each row.
+
+    for i, card in enumerate(cards):
+        rows[0] += '___'  # print the top line of the card.
+        if card == BACKSIDE:
+            #print a card's back:
+            rows[1] += '|## | '
+            rows[2] += '|###| '
+            rows[3] += '|_##| '
+        else:
+            # Print the card's front:
+            rank, suit = card # The card is a tuple data structure.
