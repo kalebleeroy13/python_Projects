3b983cfab80ae344060cc5b485aaa549fb7bf035
khuunter@yahoo.com <khuunter@yahoo.com>
2023-05-29 15:17:56 +0700

    adding counter for how many times logo bounces

--- dvdLogo.py
+++ dvdLogo.py
@@ -29,3 +29,63 @@ COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
 UP_RIGHT    = 'ur'
 UP_LEFT     = 'ul'
+DOWN_RIGHT  = 'dr'
+DOWN_LEFT   = 'dl'
+DIRECTIONS  = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+
+# Key names for logo dictionaries
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+Dir = 'direction'
+
+
+def main():
+    bext.clear()
+
+    # Generate some logos.
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+        logos.append({COLOR: random.choice(COLORS),
+                      X: random.randint(1, WIDTH - 4),
+                      Y: random.randint(1, HEIGHT - 4),
+                      DIR: random.choice(DIRECTIONS)})
+        if logos [-1][X] % 2 == 1:
+            # Make sure X is even so it can hit the corner.
+            logos[-1][X] -= 1
+
+    cornerBounces = 0 #Count how many times a logo hits a corner.
+    while True: # Main program loop.
+        for logo in logos: #handle eack logo in the logos list.
+            # Erase the logo's current location:
+            bext.goto(logo[X], logo[Y])
+            print('    ', end='') # (!)Try  commenting this line out.
+
+            originalDirectin = logo[Dir]
+
+            # See if the logo bounces pff the corners:
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[Dir] = DOWN_RIGHT
+                cornerBounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_RIGHT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[Dir] = DOWN_LEFT
+                cornerbounces += 1
+            elif logo[X] == Width - 3 and logo[Y] == Height - 1:
+                logo[DIR] = UP_LEFT
+                cornerBounces += 1
+
+            # See if the logo bounces off the left edge:
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+
+            # See if the logo bounces off the right edge:
+            #  (WIDTH - 3 because 'DVD' has 3 letters.)
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT 
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT 
