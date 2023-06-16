ab11a2649fec269d451e772306943a19f87540ae
khuunter@yahoo.com <khuunter@yahoo.com>
2023-06-16 14:25:27 +0700

    random box logic

--- carrot_in_box.py
+++ carrot_in_box.py
@@ -21,7 +21,6 @@ gets to decide if theu want to swap boxes or not.
 ''')
 input('Press Enter to begin...')
 
-
 p1Name = input('Human player 1, enter your name: ')
 p2Name = input('Human player2, enter your name: ')
 playerNames = p1Name[:11].center(11) + '  ' + p2Name[:11].center(11)
@@ -29,9 +28,29 @@ playerNames = p1Name[:11].center(11) + '  ' + p2Name[:11].center(11)
 print(''' HERE ARE TWO BOXES:
    _______________            _______________
   /              /|          /              /|
- /              / |         /              / |
 *--------------*  |        *--------------*  |
-|   RED                    |      GOLD       |
+|   RED        |           |      GOLD       |
 |   BOX        | /         | /    BOX     | /
 |              |/          |/             |/
 *--------------*           *--------------*  ''')
+
+print()
+print(playerNames)
+print()
+print(p1Name + ', you have a RED box in front of you.')
+print(p2Name + ', you have a GOLD box in front of you.')
+print()
+print(p1Name + ', you will get to look into your box.')
+print(p2Name.upper() + ', close your eyes and don\'t look !!!')
+input('When ' + p2Name + 'has closed their eyes, press Enter...')
+print()
+
+print(p1Name + 'here is the inside of your box:')
+
+if random.randint(1, 2) == 1:
+    carrotInFirstBox = True
+else:
+   carrotInFirstBox = False
+
+if carrotInFirstBox:
+   print
