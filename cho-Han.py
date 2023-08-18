b7bee4c5e755ecca18a9692e46bf9e8a332f2892
khuunter@yahoo.com <khuunter@yahoo.com>
2023-08-18 18:38:04 +0700

    intial commit

---
+++ cho-Han.py
@@ -0,0 +1,9 @@
+"""Cho-Han, by Al Sweigart al@inventwithpython.com
+the traditional Japanese dice game of even-odd.
+View this coed at https://nostarch.com/big-book-small-python-projects
+Tags:short, beginer, game"""
+
+import random, sys
+
+JAPANESE_NUMBERS ={1: 'ICHI', 2: 'NI', 3: 'SAN',
+                  4: 'SHI', 5: 'GO', 6: ROKU}
