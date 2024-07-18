024722bac879d3d6de75afb9059f931b003f00f4
khuunter@yahoo.com <khuunter@yahoo.com>
2024-07-18 12:26:02 +0800

    fixed typos & built in format method bug

--- clickbait_Generator.py
+++ clickbait_Generator.py
@@ -35,29 +35,29 @@ def main():
             break # Exit the loop once a valid number is entered.
 
     for i in range(numberOfHeadlines):
-        clickbaitType = random.randit(1, 8)
+        clickbaitType = random.randint(1, 8)
 
         if clickbaitType == 1:
             headline = generateAreMillennialsKillingHeadline()
-        elif clickBaitType == 2:
+        elif clickbaitType == 2:
             headline = generateWhatYouDontKnowHeadline()
-        elif clickBaitType == 3:
+        elif clickbaitType == 3:
             headline = generateBigCompaniesHateHerHeadline()
-        elif clickBaitType == 4:        
-            headline = generateYouWontBelieveHeadLine()
-        elif clickBaitType == 5:
+        elif clickbaitType == 4:        
+            headline = generateYouWontBelieveHeadline()
+        elif clickbaitType == 5:
             headline = generateDontWantYouToKnowHeadline()
-        elif clickBaitType == 6:
-            headline = generateGiftIdealine()
-        elif clickBaitType == 7:
-            headline = generateReasonWhyHeadline()
-        elif clickBaitType == 8:                
-            headline = generateJobAutomaedHeadline()
+        elif clickbaitType == 6:
+            headline = generateGiftIdeaHeadline()
+        elif clickbaitType == 7:
+            headline = generateReasonsWhyHeadline()
+        elif clickbaitType == 8:                
+            headline = generateJobAutomatedHeadline()
 
         print(headline)
     print()
 
-    website = random.choice(['wobsite', 'blag', 'Facebukk', 'Googles',
+    website = random.choice(['wobsite', 'blag', 'Facebuuk', 'Googles',
                             'Facesbook','Tweedie', 'Pastagram'])
     when = random.choice(WHEN).lower()
     print('Post these to our', website, when, 'or you\'re fired!')
@@ -66,10 +66,10 @@ def main():
 # Each of these functions returns a different type of headline:
 def generateAreMillennialsKillingHeadline():
     noun = random.choice(NOUNS)
-    return 'Are Millennials Killin the {} Industry?'.format(noun)
+    return 'Are Millennials Killing the {} Industry?'.format(noun)
 
 def generateWhatYouDontKnowHeadline():
-    noun = Random.choice(NOUNS)
+    noun = random.choice(NOUNS)
     pluralNoun = random.choice(NOUNS) + 's'
     when = random.choice(WHEN)
     return 'Without This {}, {} Could kill you {}'.format(noun, pluralNoun, when)
@@ -77,55 +77,51 @@ def generateWhatYouDontKnowHeadline():
 
 
 def generateBigCompaniesHateHerHeadline():
-    pronoun = randomchoice(OBJECT_PRONOUNS)
+    pronoun = random.choice(OBJECT_PRONOUNS)
     state = random.choice(STATES)
     noun1 = random.choice(NOUNS)
     noun2 = random.choice(NOUNS)
-    return 'Big Companies Hate {}! See How this {} {} Invented a Cheaper {}'.format
-    (pronoun, state , noun1, noun2)
+    return 'Big Companies Hate {}! See How this {} {} Invented a Cheaper {}'.format(pronoun, state , noun1, noun2)
 
 def generateYouWontBelieveHeadline():
     state = random.choice(STATES)
     noun = random.choice(NOUNS)
-    pronoun = random.choice(POSSEIVE_PRONOUNS)
-    return 'You Won\'t Believe What This {} {}Found in {} {}'.format
-    (state, noun, pronoun, place)
+    pronoun = random.choice(POSSESIVE_PRONOUNS)
+    place = random.choice(PLACES)
+    return 'You Won\'t Believe What This {} {}Found in {} {}'.format(state, noun, pronoun, place)
 
 
 def generateDontWantYouToKnowHeadline():
-    pluralNoun1 = random.coice(NOUNS) + 's'
-    pluralNoun2 = random.coice(NOUNS) + 's'
+    pluralNoun1 = random.choice(NOUNS) + 's'
+    pluralNoun2 = random.choice(NOUNS) + 's'
     return 'What {} Don\'t Want you To Know About {}'.format(pluralNoun1, pluralNoun2)
 
 
-def generateGiftIdeaHeadLine():
-    numer = random.randint(7, 15)
+def generateGiftIdeaHeadline():
+    number = random.randint(7, 15)
     noun = random.choice(NOUNS)
     state = random.choice(STATES)
-    return '{} Gift Ideas To Give Your {} From {}'/format(number, noun, state)
+    return '{} Gift Ideas To Give Your {} From {}'.format(number, noun, state)
 
 
 def generateReasonsWhyHeadline():
-    number1 = random.tandint(3, 19)
+    number1 = random.randint(3, 19)
     pluralNoun = random.choice(NOUNS) + 's'
     # number2 should no be no larer than number1:
     number2 = random.randint(1, number1)
-    return '{} Reasons Why {} Are More Interesting Than You Think ( Number {} Will \
-    Suprise You!)'.format(number1, pluralNoune, number2)
+    return '{} Reasons Why {} Are More Interesting Than You Think ( Number {} Will Suprise You!)'.format(number1, pluralNoun, number2)
 
-def GenerateJobAutomatedHeadline():
-    state = radnom.choice(STATES)
+def generateJobAutomatedHeadline():
+    state = random.choice(STATES)
     noun = random.choice(NOUNS)
 
     i = random.randint(0, 2)
     pronoun1  = POSSESIVE_PRONOUNS[i]
     pronoun2 = PERSONAL_PRONOUNS[i]
     if pronoun1 == 'Their':
-        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Were \
-        Wrong.'.format(state, noun, pronoun1, pronoun2)
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Were Wrong.'.format(state, noun, pronoun1, pronoun2)
     else:
-        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Was \
-        Wrong.'.format(state, noun, pronoun1, pronoun2)
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Was Wrong.'.format(state, noun, pronoun1, pronoun2)
 
 
 # if the program is run (instead of imported) run the game:
