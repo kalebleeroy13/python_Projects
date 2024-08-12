ecce4d6ff61b15a6e81ab235cafdf556f74b8aaf
khuunter@yahoo.com <khuunter@yahoo.com>
2024-08-12 21:00:08 +0800

    script finished

---
+++ collatz_Sequence.py
@@ -0,0 +1,40 @@
+"""Collatz Sequence, by Al Sweigart al@investwithpython.com
+Generates numbers for the Collatz sequence, given a starting number.
+More info at: https://en.wilkipedia.org/wiki/collatz_comjecture
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, math"""
+
+import sys, time
+
+print('''Collatz Sequence, or, the #n +1 problem
+By Al Sweigart al@inventwithpython.com
+
+The Collatz sequence is a sequence of numbers produced from a starting 
+number n, following three rules:
+
+1) If n is even, the next number n is n / 2.
+2) If n is odd, the next number n is n * 3 + 1.
+3) If n is 1, stop. Otherwise, repeat.
+
+it is generally thought, but so far not mathematically proven, that
+every starting number eventually terminates at 1.
+''')
+
+print('Enter a starting number (greater than 0) or QUIT:')
+respone = input('>')
+
+if not response.isdecimal()or response == '0':
+    print('You must enter an interger greater than ')
+    sys.exit()
+
+n = int(response)
+print(n, end='', flush=True)
+while n != 1:
+    if n % 2 == 0: #if n is even ...
+        n = n // 2
+    else: # Otherwise, n is odd... 
+        n = 3 * n + 1
+
+    print(', ' + str(n), end='', flush=True)
+    time.sleep(0.1)
+print()
