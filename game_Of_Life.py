40e718a90e42d35cd72731a15af3d4174a5bb32f
khuunter@yahoo.com <khuunter@yahoo.com>
2024-08-13 13:02:43 +0800

    intial commit

---
+++ game_Of_Life.py
@@ -0,0 +1,6 @@
+"""Conway's Game of Life, by Al Sweigart al@inventwithpython.com
+The classic cellular automata simulation. Press Ctrl-C to stop.
+More infor at: https://nostarch.com/big-book-small-python-projects
+Tags: short, artistic, simulation"""
+
+import copy, random, sys, time
