688b9addd23ff7c0baae2a3530f48677665bf460
khuunter@yahoo.com <khuunter@yahoo.com>
2024-11-07 10:30:07 +0700

    typo fix

--- deepCave.py
+++ deepCave.py
@@ -20,19 +20,19 @@ gapWidth = 10
 while True:
     # Display the tunnel segment:
     rightWidth = WIDTH - gapWidth - leftWidth
-    print(('#' * leftWidth) + ('' * gapWidth) + ('#' * rightWidth))
+    print(('#' * leftWidth) + (' ' * gapWidth) + ('#' * rightWidth))
 
     # Check for Ctrl-C press during the brief pause:
     try:
         time.sleep(PAUSE_AMOUNT)    
-    except keyboardInterrupt:
+    except KeyboardInterrupt:
         sys.exit()  # When Ctrl-c is pressed, end the program.
 
     # Adjust the left side width:
     diceRoll = random.randint(1, 6)
     if diceRoll == 1 and leftWidth > 1:
         leftWidth = leftWidth -1  # Decrease left side width.
-    elif diceRoll == 2 and leftWidth + gapWidth < Width - 1:
+    elif diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
         leftWidth = leftWidth + 1 # Increse left side width.
     else:
         pass # Do nothing; no change in left side width.
