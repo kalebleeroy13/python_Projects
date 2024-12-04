87fc819a179063eafb74e95e43a9f99d15e51579
khuunter@yahoo.com <khuunter@yahoo.com>
2024-12-04 09:41:45 -0500

    main loop logic

--- matrixStream.py
+++ matrixStream.py
@@ -11,3 +11,37 @@ DENSITY = 0.02 # (!) try changing this to 0.10 or 0.30
 
 # Get the size of the terminal window:
 WIDTH = shutil.get_terminal_size()[0]
+
+# can't print to the last column on Windows without it adding 
+# a newline automatically, so reduce the width by one:
+WIDTH -= 1
+
+print('Matrix Stream')
+print('press Ctrl-C to quit.')
+time.sleep(2)
+
+try:
+    # For each column, when the counter is 0, no stream is shown
+    # Otherwise, it acts as a counter for how many times a 1 or 0 
+    # should be displayed in that column.
+    columns = [0] * WIDTH
+    while True:
+        # Set up the counter for each column:
+        for i in range(WIDTH):
+            if columns[i] == 0:
+                if random.random() <= DENSITY:
+                    # Restart a stream on this column.
+                    column[i] = rndom.randint(MIN_STREAM_LENGTH, 
+                                                MAX_STREAM_LENGTH)
+
+                # Display an empty space or a 1/0 character.
+                if columns[i] > 0:
+                    print(random.choice(STREAM_CHARS), end='')
+                    columns[i] -= 1
+                else:
+                    print(' ', end='')
+                print() #print a newline at the end of the row of columns.
+                sys.stdout.flush() # Make sure text appears on the screen.
+                time.sleep(PAUSE)
+except KeyboardInterrupt:
+    sys.ecit() # When Ctrl-C is hit , end program
