a019ba84e045702474aed3786f06eb99d169d1cb
khuunter@yahoo.com <khuunter@yahoo.com>
2024-12-04 17:44:54 -0500

    refactor comments

--- matrixStream.py
+++ matrixStream.py
@@ -1,10 +1,10 @@
 import random, shutil, sys, time
 
-# Set up the constanst:
-MIN_STREAM_LENGTH = 6 #(!) Try changing this to 1 or 50.
-MAX_STREAM_LENGTH = 14 # (!) Try changing this to 100.
-PAUSE = 0.1 # (!) Try changing this to 0.0 or 2.0.
-STREAM_CHARS = ['0', '1'] # (!) Try changing this to other characters.
+# Set up the constansts:
+MIN_STREAM_LENGTH = 6 # min distance
+MAX_STREAM_LENGTH = 20 # max distance
+PAUSE = 0.1 # changes speed of stream
+STREAM_CHARS = ['0', '1'] # characters of the stream
 
 # Density can range from 0.0 to 1.0
 DENSITY = 0.02 # (!) try changing this to 0.10 or 0.30
@@ -14,7 +14,7 @@ WIDTH = shutil.get_terminal_size()[0]
 
 
 print('Matrix Stream')
-print('press Ctrl-C to quit.')
+print('Press Ctrl-C to quit.')
 time.sleep(2)
 
 try:
@@ -37,7 +37,7 @@ try:
                 columns[i] -= 1
             else:
                 print(' ', end='')
-                
+
         print() #print a newline at the end of the row of columns.
         sys.stdout.flush() # Make sure text appears on the screen.
         time.sleep(PAUSE)
