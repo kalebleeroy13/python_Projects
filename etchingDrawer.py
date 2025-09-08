d285f4ee297b9cdcc8a054f63c3de09e0189a5a6
khuunter@yahoo.com <khuunter@yahoo.com>
2025-09-08 13:54:08 -0400

    bug and typo fix

--- etchingDrawer.py
+++ etchingDrawer.py
@@ -2,7 +2,7 @@
 WASD keys. Inspired by Etch A Sketch toys.
 
 For example, you can draw Hilbert Curve fractal with:
-SDWDDSASDSAAWASSDSASSDWDSDWWAWDDDSASSDWDSDWWAWDWWASAAWDWAWDDSDW
+
 
  larger Hilbert Curve fractal with:
 DDSAASSDDWDDSDDWWAAWDDDDSDDWDDDDSAASDDSAAAAWAASSSDDWDDDDSAASDDSAAAAWA
@@ -28,13 +28,13 @@ CROSS_CHAR              = chr(9532) # Character 9532 is '┼'
 
 # Get the size of the terminal window:
 CANVAS_WIDTH, CANVAS_HEIGHT =shutil.get_terminal_size()
-# We can't print to the last column on Window withouth it adding a 
-# newline automaitcally, so reduve the width by one:
+# We can't print to the last column on Window without it adding a 
+# newline automaitcally, so reduce the width by one:
 CANVAS_WIDTH -= 1
 # leave room at the bottom few rows for the command info lines. 
 CANVAS_HEIGHT -= 5
 
-""" The keys for canvas willl be (x, y) interger tuples for the coordinate,
+""" The keys for canvas will be (x, y) interger tuples for the coordinate,
 and the value is a set of letterss W, A, S, D that tell what kind of line
  should be drawn."""
 canvas = {}
@@ -43,7 +43,7 @@ cursorY = 0
 
 
 def getCanvasString(canvasData, cx, cy):
-    """Rturns a multiline string of line drawn in canvasData."""
+    """Returns a multiline string of line drawn in canvasData."""
     canvasStr = ''
 
     """canvasData is a dictionary with (x,y) tuple keys and values that
@@ -54,29 +54,30 @@ def getCanvasString(canvasData, cx, cy):
             if columnNum == cx and rowNum == cy:
                 canvasStr += '#'
                 continue
+
             # Add the line character for this point to canvasStr.
             cell = canvasData.get((columnNum, rowNum))
             if cell in (set(['W', 'S']), set(['W']), set(['S'])):
                 canvasStr += UP_DOWN_CHAR
             elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
                 canvasStr += LEFT_RIGHT_CHAR
-            elif cell in (set(['S', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['S', 'D']):
                 canvasStr += DOWN_RIGHT_CHAR
-            elif cell in (set(['A', 'S']), set(['A']), set(['D'])):
+            elif cell in set(['A', 'S']):
                 canvasStr += DOWN_LEFT_CHAR
-            elif cell in (set(['W', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'D']):
                 canvasStr += UP_RIGHT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'A']):
                 canvasStr += UP_LEFT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'S','D']):
                 canvasStr += UP_DOWN_RIGHT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'S', 'A']):
                 canvasStr += UP_DOWN_LEFT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['A', 'S', 'D']):
                 canvasStr += DOWN_LEFT_RIGHT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'A', 'D']):
                 canvasStr += UP_LEFT_RIGHT_CHAR
-            elif cell in (set(['A', 'D']), set(['A']), set(['D'])):
+            elif cell in set(['W', 'A', 'S', 'D']):
                 canvasStr += CROSS_CHAR
             elif cell == None:
                 canvasStr += ' '
@@ -99,7 +100,7 @@ while True:  # Main progam loop.
     elif response == 'H':
         print('Enter W, A , S, and D characters to move the cursor and')
         print('draw a line behind it as it moves. for example ddd')
-        print('draws a lome going right and sssdddwwwaa draws a box.')
+        print('draws a line going right and sssdddwwwaa draws a box.')
         print()
         print('You can save your drawing to a text file by entering F.')
         input('Press Enter to return to the program...')
@@ -168,6 +169,3 @@ while True:  # Main progam loop.
             canvas[(cursorX, cursorY)].add('D')
         elif command == 'D':
             canvas[(cursorX, cursorY)].add('A')
-
-
-
