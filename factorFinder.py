f81da2a4fb74e2aad2061eda52016100b56fb6a9
khuunter@yahoo.com <khuunter@yahoo.com>
2025-09-11 18:05:04 -0400

    intro

---
+++ factorFinder.py
@@ -0,0 +1,14 @@
+import math, sys
+
+print('''Factor Finder, by Al Sweigart al@inventwithpython.com
+      
+A number's factor are two numbers that, when multiplied with each
+other, produce the number. For example, 2 x 13 = 26, so 2 and 13 are
+factors of 26. 1 x 26 = 26, so 1 and 26 are also factors of 26. We
+say that 26 has four factors: 1, 2, 13, and 26.
+
+if a number only has two factors (1 and itself), we call that a prime
+number. Otherwise, we call it a composite number.
+      
+Can you discover some prime numbers?
+''')
