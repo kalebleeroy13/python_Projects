bb523e5dfcbdfdbf8b7f0c15d6937476936cff0d
khuunter@yahoo.com <khuunter@yahoo.com>
2025-09-29 13:36:50 +0200

    program  explanation

---
+++ fibonacciSequence.py
@@ -0,0 +1,7 @@
+import sys
+
+print('''Fibonaccci sequence begins with 0 and 1, and the next number is the sum of the previous two numbers.
+      The seuence continues forever:
+      
+     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987...''' )
+
