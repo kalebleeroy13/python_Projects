0cc4a4923ae10c9b810d2231b66accf7bf691227
khuunter@yahoo.com <khuunter@yahoo.com>
2025-10-01 09:51:10 +0200

    constants

--- fishTank.py
+++ fishTank.py
@@ -1,1 +1,23 @@
-''''''
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instruction at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up constants:
+WIDTH, HEIGHT = bext.size()
+# Can't print to the last column on Windows without it adding a 
+# newline automatically, so reduxe the width by one:
+WIDTH -= 1
+
+NUM_KELP = 2 # (!) TRY CHANGING THIS TO 10.
+NUM_FISH = 10 # (!) TRY changing this to 2 or 100
+NUM_BUBBLERS = 1 # (!) Try changing this to 0 or 10.
+FRAMES_PER_SECOND = 4 # (!) try changing this number to 1 or 60.
+# (!) try changing the constants to create a fish tank with only kelp.
+# or only bubblers.
+
